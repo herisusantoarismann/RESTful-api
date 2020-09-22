d24bc7d6d12d0bc431dbc7391d8b6b649c895c80
Heri Susanto Arisman <eriparigi12@gmail.com>
2020-09-22 13:21:45 +0700

    Update todoListRoutes.js

--- RESTful_API/api/routes/todoListRoutes.js
+++ RESTful_API/api/routes/todoListRoutes.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   var todoList = require("../controllers/todoListController");
 
   // todoList Routes
-  app.route("/tasks").get(todoList.list).post(todoList.create);
+  app.route("/kontaks").get(todoList.list).post(todoList.create);
 
-  app.route("/tasks/:id").put(todoList.update).delete(todoList.delete);
+  app.route("/kontak/:id").put(todoList.update).delete(todoList.delete);
 };
