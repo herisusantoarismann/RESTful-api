daaf0bb974a699f92ea0579484d1175f84d358d7
Heri Susanto Arisman <eriparigi12@gmail.com>
2020-09-22 17:09:41 +0700

    Adding Cors

--- RESTful_API/server.js
+++ RESTful_API/server.js
@@ -1,9 +1,10 @@
 var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 5000,
   mongoose = require("mongoose"),
   Telepon = require("./api/models/KontakModel"), //created model loading here
-  bodyParser = require("body-parser");
+  bodyParser = require("body-parser"),
+  cors = require("cors");
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -11,10 +12,11 @@ mongoose.connect("mongodb://localhost/kontak");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var routes = require("./api/routes/KontakRoutes"); //importing route
 routes(app); //register the route
 
 app.listen(port);
 
-console.log("todo list RESTful API server started on: " + port);
+console.log("Kontak RESTful API server started on: " + port);
